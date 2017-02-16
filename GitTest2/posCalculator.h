75c07dd4c210282e789bcd53631cedd1bfafde2f
Andrew Bourhis <andrewbourhis@gmail.com>
2017-02-15 20:57:16 -0500

    I modified posCalculator.cpp to include some comments to help you out. I also created a header file for the posCalculator.cpp because this is no longer the main file and we desire to call functions from the mainwindow.cpp file. The idea is to include this header in mainwindow, then within the eventhandler of button presses, we just call the public functions of posCalculator like calibrateSensors() or beginTracking(). This will require writing these functions from the code that is contained in the while loop in posCalculator.cpp (which we can work on together if you need help).

---
+++ GitTest2/posCalculator.h
@@ -0,0 +1,16 @@
+#ifndef POSCALCULATOR_H
+#define POSCALCULATOR_H
+
+class POSCALCULATOR_H
+{
+public:
+    //should likely break up these functions within the mainfn into things like calibrate(), acquireCoVar(), beginTracking(), etc...
+    //and then make all other functions private
+    int mainfn();
+
+private:
+
+};
+
+
+#endif // POSCALCULATOR_H
